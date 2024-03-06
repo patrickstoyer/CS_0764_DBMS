bffc47d0952ca7debe0b5002c466a04d8945bcd0
Patrick Stoyer <stoyerp8991@gmail.com>
2024-03-06 09:14:30 -0600

    Update Tasks Add Record constructor from file

--- Record.cpp
+++ Record.cpp
@@ -1,11 +1,26 @@
 #include "Record.h"
 #include <string.h>
+#include <fstream>
 
-Record::Record(unsigned char * key, unsigned char * pointer)
+Record::Record(unsigned char * key, 
+    unsigned char level, unsigned char file, unsigned int which)
 {
 	TRACE (true);
     this->key = key;
-    this->pointer = pointer;
+    this->level = level;
+    this->file = file;
+    this->which = which;
+}
+
+Record::Record(fstream File
+    unsigned char level, unsigned char file, unsigned int which)
+{
+	TRACE (true);
+    char * line = File.getline();
+    this->key = line[constants::KEY_SIZE];
+    this->level = level;
+    this->file = file;
+    this->which = which;
 }
 
 Record::~Record ()
--- Record.h
+++ Record.h
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <fstream>
 #include "defs.h"
 
 class Record
 {
 public:
 	Record (unsigned char * key, unsigned char * pointer);
+    Record (fstream File); // Creates a record from next line of file
 	virtual ~Record ();
 	bool sortsBefore(Record * other); // Called to check sort order
+    void storeRecord (fstream File);
 private:
     unsigned char * key; // An array representing the key (variable size, passed as param)
     unsigned char * pointer; // A pointer to the record in memory (TODO: should this instead be three things level (e.g. SSD runs) + part (e.g. 3rd run) + offset (e.g. 100th entry))
